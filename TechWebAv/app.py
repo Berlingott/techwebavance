a2d788302f187d22200e4244413725f64638d7b5
Simon Duchesne <berlingott@MacBook-Pro-de-Simon.local>
2022-02-14 13:05:34 -0500

    Add __init__.py for app and import

--- TechWebAv/app.py
+++ TechWebAv/app.py
@@ -3,10 +3,10 @@ This script runs the application using a development server.
 It contains the definition of routes and views for the application.
 """
 
-from flask import Flask
-app = Flask(__name__)
+from website import create_app
 
 # Make the WSGI interface available at the top level so wfastcgi can get it.
+app = create_app()
 wsgi_app = app.wsgi_app
 
 
