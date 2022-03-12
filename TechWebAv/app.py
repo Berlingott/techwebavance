50793c39597c01d9d2486640c947e0fe2a2ec431
berlingott <simsimondu@gmail.com>
2022-03-12 15:37:01 -0500

    add template folder add auth python file add views file create home template

--- TechWebAv/app.py
+++ TechWebAv/app.py
@@ -6,17 +6,12 @@ It contains the definition of routes and views for the application.
 from website import create_app
 
 # Make the WSGI interface available at the top level so wfastcgi can get it.
-app = create_app()
+app = create_app()  # create_app est declarer dans le fichier __init__.py
 wsgi_app = app.wsgi_app
-
-
-@app.route('/')
-def hello():
-    """Renders a sample page."""
-    return "Hello Simon!"
 
 if __name__ == '__main__':
     import os
+
     HOST = os.environ.get('SERVER_HOST', 'localhost')
     try:
         PORT = int(os.environ.get('SERVER_PORT', '5555'))
--- TechWebAv/website/__init__.py
+++ TechWebAv/website/__init__.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask  # Importation de toutes les librairies necessaire au projet
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
 
 
-def create_app():
+def create_app():  # Initialisation de l'application
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "berlingotestlaclefmaisavecun0alafin"
 
-    return app
+    from .views import views  # importation de nos fichiers cree, le point est parce qu'ils font partie du projet
+    from .auth import auth
+    app.register_blueprint(views, url_prefix="/")
+    app.register_blueprint(auth, url_prefix="/")
+
+    return app  # On retourne l'application
