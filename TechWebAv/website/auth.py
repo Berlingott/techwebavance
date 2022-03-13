f8107ddef6e753ee2d63c8db735c6d85fdbd58b1
berlingott <simsimondu@gmail.com>
2022-03-12 21:10:48 -0500

    signup and login page

--- TechWebAv/website/auth.py
+++ TechWebAv/website/auth.py
@@ -1,15 +1,16 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, redirect, url_for
 
 auth = Blueprint("auth", __name__)
 
 @auth.route("/login")
 def login():
-    return "Login"
+    return render_template("login.html")
 
 @auth.route("/signup")
 def signup():
-    return "signup"
+    return render_template("signup.html")
+
 
 @auth.route("/logout")
 def logout():
-    return "logout"
+    return redirect(url_for("views.home"))
