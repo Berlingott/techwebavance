90c5d04ea8b6f8f7d3d3a837ca038e4031e13935
berlingott <simsimondu@gmail.com>
2022-03-19 20:46:00 -0400

    Fix Author of the article issue on the footer of the card

--- TechWebAv/website/tables.py
+++ TechWebAv/website/tables.py
@@ -11,7 +11,7 @@ from sqlalchemy import Table, Column, Integer, String, MetaData
 
 class Usagers(database.Model, UserMixin):
     __tablename__ = 'Usagers'
-    id = database.Column(database.INTEGER, primary_key=True, autoincrement=True)
+    id = database.Column(database.Integer, primary_key=True, autoincrement=True)
     nom = database.Column(database.String(100))
     username = database.Column(database.String(100), unique=True)
     password = database.Column(database.String(100))
@@ -21,7 +21,7 @@ class Usagers(database.Model, UserMixin):
 
 class Articles(database.Model):
     __tablename__ = 'Articles'
-    id = database.Column(database.INTEGER, primary_key=True, autoincrement=True)
+    id = database.Column(database.Integer, primary_key=True, autoincrement=True)
+    usagers_id = database.Column(database.Integer, database.ForeignKey('Usagers.id', ondelete='CASCADE'), nullable=False)
     textArticle = database.Column(database.String(254))
     datePublication = database.Column(database.DateTime(timezone=True), default=func.now())
-    usagers_id = database.Column(database.INTEGER, database.ForeignKey('Usagers.id', ondelete='CASCADE'), nullable=False)
