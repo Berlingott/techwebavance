403230cb4f9024f85f13c5f7d0442d4ed8fe70dc
berlingott <simsimondu@gmail.com>
2022-03-21 23:02:46 -0400

    ajout des tests

---
+++ TechWebAv/website/tests/conftest.py
@@ -0,0 +1,31 @@
+import pytest
+from app import create_app
+from website.tables import Usagers
+from . import database
+
+## scope:
+### function: pour chaque fonction de test, pytest créera une nouvelle application et la supprimera à la fin
+### module: créer une seule application à utiliser dans toutes les fonctions de test
+
+@pytest.fixture(scope="module")
+def app():
+    """Instance of Main flask app"""
+    return create_app()
+
+
+
+@pytest.fixture(scope='module')
+def new_user():
+    user = User(email='patkennedy79@gmail.com', username='FlaskIsAwesome')
+    return user
+
+@pytest.fixture(scope='module')
+def init_database():
+    app = create_app()
+    client = app.test_client()
+    with app.app_context:
+        user = User(username="foobar", email="foo@bar.com")
+        db.session.add(user)
+        db.session.commit()
+    yield client
+
